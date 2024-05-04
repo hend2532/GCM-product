35440ab42ddc9e41bd7000cb7e891145129cabc7
norhan-nabil <nabilnorhan324@gmail.com>
2024-05-03 17:34:21 -0700

    star backend (categry model & category api)

---
+++ nodejs/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import categoryRoute from './routes/categoryRoute.js'
+
+const app=express();
+
+//use Routes
+app.use('/api/categories' , categoryRoute);
+
+
+//connect to database
+mongoose.connect("mongodb+srv://norhan:123456789n@gcm.80yheul.mongodb.net/?retryWrites=true&w=majority&appName=GCM")
+.then( () =>{console.log("cccoonneeccttteeddd")})
+.catch( (err) =>{console.log(err)})
+
+
+//listen
+app.get('/n', (req,res) =>{
+    res.send("nnbbbbbnn")
+});
+
+
+app.listen(3000);
